037c542cc8073d6ef287cc0657dc89e92d41b7a1
vitalina <vitalina.zubko@mev.com>
2023-12-20 20:51:07 +0200

    updated playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -28,7 +28,7 @@ const config = defineConfig({
     ['list']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    headless: false,
+    headless: true,
     httpCredentials: testConfig.httpCredentials,
     screenshot: 'only-on-failure',
     /* Base URL to use in actions like `await page.goto('/')`. */
